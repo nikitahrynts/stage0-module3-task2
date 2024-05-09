9fe002e267e4b2322a8fdc1c27aedbc8b24d7ab6
Nikita <hrynts2002@gmail.com>
2024-05-10 02:07:17 +0300

    Tasks finished in accordance with requirements

--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -2,10 +2,10 @@ package school.mjc.stage0.module3.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variable = 10;
-        int variable_one = 1;
+        int _variable = 10;
+        int variable_One = 1;
         int variableTwo = 2;
-        int variable_three = 3;
+        int variableThree = 3;
         final int VARIABLE_FOUR = 4;
         final int VARIABLE_FIVE = 5;
         int variableSix = 6;
